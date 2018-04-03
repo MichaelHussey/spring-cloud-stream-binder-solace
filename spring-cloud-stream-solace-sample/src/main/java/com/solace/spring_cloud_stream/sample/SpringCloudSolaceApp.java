3522bb7dc865a5320dba5518c007b30012e42458
Mic Hussey <Michael.Hussey@solace.com>
2018-04-03 19:11:30 +0200

    Removed unused code

--- spring-cloud-stream-solace-sample/src/main/java/com/solace/spring_cloud_stream/sample/SpringCloudSolaceApp.java
+++ spring-cloud-stream-solace-sample/src/main/java/com/solace/spring_cloud_stream/sample/SpringCloudSolaceApp.java
@@ -2,10 +2,8 @@ package com.solace.spring_cloud_stream.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
-import org.springframework.integration.annotation.Transformer;
 
 import com.solace.spring_cloud_stream.binder.SolaceBinderConfiguration;
 
@@ -13,14 +11,6 @@ import com.solace.spring_cloud_stream.binder.SolaceBinderConfiguration;
 @Import(SolaceBinderConfiguration.class)
 public class SpringCloudSolaceApp {
 	
-	/**
-	@Transformer(inputChannel = Processor.INPUT, outputChannel = Processor.OUTPUT)
-	public String transform(String in) {
-		return in + " world";
-	}*/
-
-    static final String LISTEN_TOPIC = "test/>";
-
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringCloudSolaceApp.class, args);
 	}
